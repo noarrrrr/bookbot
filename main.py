a1d4755c8a5e35d39baa5975b2e6e239ee1131fc
Noah Patton <noah.patton1225@gmail.com>
2025-12-13 05:57:36 -0800

    Oh yea baby this clanker counts shiz

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+
+try:
+    PATH = sys.argv[1]
+except Exception:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
 def get_book_text(path):
     with open(path) as f:
         content = f.read()
@@ -7,10 +15,21 @@ from stats import wordcount
 
 from stats import countcharacters
 
+from stats import sort_key
+
+from stats import explode_and_sort
+
 def main():
-    num_words = wordcount(get_book_text("books/frankenstein.txt"))
-    charcount = countcharacters(get_book_text("books/frankenstein.txt"))
-    print(f'Found {num_words} total words')
-    print(charcount)
+    num_words = wordcount(get_book_text(PATH))
+    charcount = explode_and_sort(countcharacters(get_book_text(PATH)))
+    
+    print(f'''============ BOOKBOT ============
+Analyzing book found at {PATH}
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------''')
+    for i in charcount:
+        print(i['char']+':', i['num'])
+    
     
 main()
--- stats.py
+++ stats.py
@@ -5,23 +5,31 @@ def wordcount(raw_text):
         count += 1
     return count
 
-raw_text = '1 2 3 4 5 A a A b C d E @ #'
+test_text = '1 2 3 4 5 A a A b B C d E e e e @ #'
 
 def countcharacters(text):
     dict = {}
     lowercase_text = text.lower()
     for i in list(lowercase_text):
-        if i == ' ':
-            pass
-        elif i in dict:
-            dict[i] += 1
-        else:
-            dict[i] = 1
+        if i.isalpha():
+            if i in dict:
+                dict[i] += 1
+            else:
+                dict[i] = 1
     
     return dict
 
-def test():
-    result = countcharacters(raw_text)
-    print(result)
+def sort_key(unsorted_list):
+    return unsorted_list['num']
 
-test()
+def explode_and_sort(d):
+    sorted = []
+    for i in d:
+        sorted.append({'char': i, 'num': d[i]})
+    sorted.sort(reverse=True, key=sort_key)
+    return sorted
+
+def test():
+    sorted = explode_and_sort(countcharacters(test_text))
+    print(sorted)
+
